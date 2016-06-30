67fe3d4e7365e2d78f9743f8af909a9f492bf9bd
Pankaj Lal <pankaj.r.lal@gmail.com>
2016-06-30 12:04:31 +0530

    updated for the postgresql backend

--- prabandh/settings.py
+++ prabandh/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'prabandh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PRABANDH_PG_DATABASE'),
+        'USER': os.environ.get('PRABANDH_PG_USER'),
+        'PASSWORD': os.environ.get('PRABANDH_PG_PASSWD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
